668cae8708e84171cdaf367c1815ac40b28fa13f
Blessing <ogorblessing96@gmail.com>
2024-11-14 15:16:36 +0100

    Backend user data

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+from flask import request
+from typing import List, TypeVar
+
+
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path is None or excluded_paths is None:
+            return True
+        if path in excluded_paths:
+            return False
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        if request is None:
+            return None
+        if request.headers.get("Authorization") is None:
+            return None
+        return request.headers.get("Authorization")
+    
+    def current_user(self, request=None) -> TypeVar("User"):
+        return None
